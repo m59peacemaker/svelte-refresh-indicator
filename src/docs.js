b79b8e0d5f318dae6e21deca827b0c4bba1cb9ed
Johnny Hauser <johnnyhauser@gmail.com>
2017-08-06 22:24:00 -0500

    added "emphasized" option for plain spinner or spinner in emphasized container

--- src/docs.js
+++ src/docs.js
@@ -4,13 +4,13 @@ const makeIndicator = data => {
   const div = document.createElement('div')
   div.style.margin = '30px 50px'
   div.style.display = 'inline-block'
-  div.style.verticalAlign = 'top'
+  div.style.verticalAlign = 'middle'
   document.body.appendChild(div)
   return new Indicator({ target: div, data })
 }
 
 const demoStep = (indicator, ratio, resetFn) => {
-  if (ratio > 1.4) { return resetFn() }
+  if (ratio > 1.8) { return resetFn() }
   window.requestAnimationFrame(() => {
     indicator.set({ progressRatio: ratio + 0.01 })
     demoStep(indicator, ratio + 0.01, resetFn)
@@ -27,6 +27,15 @@ makeIndicator()
 
 {
   const indicator = makeIndicator()
+  const resetFn = () => {
+    setTimeout(() => indicator.set({ progressRatio: undefined }), 100)
+    setTimeout(() => demoStep(indicator, 0, resetFn), 3300)
+  }
+  demoStep(indicator, 0, resetFn)
+}
+
+{
+  const indicator = makeIndicator({ size: 75, emphasized: true })
   const resetFn = () => {
     setTimeout(() => indicator.set({ progressRatio: undefined }), 100)
     setTimeout(() => demoStep(indicator, 0, resetFn), 3300)
